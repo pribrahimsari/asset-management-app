3241adf1273602f98e0ea1855296739d834cf120
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-13 23:33:43 +0300

    fix: fillable content of Asset model

--- api/app/Models/Asset.php
+++ api/app/Models/Asset.php
@@ -9,6 +9,8 @@ class Asset extends Model
 {
     use HasFactory;
 
+    protected $fillable = ["name", "description", "type_id", "addition_time", "priority"];
+
     // one-to-one relation between
     public function type() {
         return $this->belongsTo(Type::class);
