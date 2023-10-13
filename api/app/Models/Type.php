10dbc40e523d8a973b382e0e4dea7ce5558c53f5
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 00:00:16 +0300

    fix softDelete: add missing softDeletes definitions in models

--- api/app/Models/Type.php
+++ api/app/Models/Type.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Type extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     public function assets() {
         return $this->hasMany(Asset::class);
