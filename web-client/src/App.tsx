ec43d2e9c09a185bc63428c37d5ad09e83f0e3f9
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 15:30:00 +0300

    primitive onScroll listener/handler

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,6 +3,7 @@
 // console.debug({ ANY_API_URL });
 
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const fetchAssets = async () => {
   const response = await fetch("http://localhost:8000/v1/assets?page=1");
@@ -16,6 +17,40 @@ const App = () => {
   });
 
   console.debug({ data });
+
+  // adds eventListener to on scroll of document
+  // triggers fetching after scroll calculations
+  useEffect(() => {
+    // instead of useQuery's loading state, this can be faster
+    let fetching = false;
+
+    const onScroll = (event: Event) => {
+      // scrollHeight: whole content height
+      // scrollTop: how far away from content's top / other saying: how you did scroll
+      // clientHeight: todo
+      const { scrollHeight, scrollTop, clientHeight } = event.target.scrollingElement;
+
+      // if (
+      //   window.innerHeight + document.documentElement.scrollTop !==
+      //   document.documentElement.offsetHeight /*|| isLoading*/
+      // ) {
+      //   return;
+      // }
+      //   todo: better calculation
+
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+        fetching = true;
+        console.log("trigger fetch");
+        fetching = false;
+      }
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, []);
 
   return (
     <main>
