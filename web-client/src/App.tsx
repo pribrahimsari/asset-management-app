86e5afe740080029a938a177eb5b90b4bd6def09
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 14:30:12 +0300

    clean code base -remove redundant files and codes

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,16 +1,13 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-
 // todo: import from ENV when needed
 // const ANY_API_URL = import.meta.env.VITE_ANY_API_URL;
 // console.debug({ ANY_API_URL });
 
-function App() {
-  // const [count, setCount] = useState(0);
-
-  return <>Asset Management App</>;
-}
+const App = () => {
+  return (
+    <main>
+      <h1>Asset Management Application</h1>
+    </main>
+  );
+};
 
 export default App;
