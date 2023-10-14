1a6a1133e69543f91b8b66a5b12625bd07d79450
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 23:17:40 +0300

    use Layout in App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,10 +1,59 @@
 import AssetList from "src/components/AssetList.tsx";
+import { Box } from "@mui/joy";
+import { makeStyles } from "tss-react/mui";
+import Header from "src/components/Layout/Header.tsx";
+import Sidebar from "src/components/Layout/Sidebar.tsx";
+import StickySubHeader from "src/components/Layout/StickySubHeader.tsx";
+
+// thanks to TSS-React lib for CSS in TS solution as in MUI v4
+const useStyles = makeStyles()(() => ({
+  mainBox: {
+    pt: {
+      xs: "calc(12px + var(--Header-height))",
+      md: 3,
+    },
+    pb: {
+      xs: 2,
+      sm: 2,
+      md: 3,
+    },
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    minWidth: 0,
+    // height: "100dvh", // <-- breaks InfiniteScrolling
+    gap: 1,
+    overflow: "auto",
+  },
+}));
 
 const App = () => {
+  const { classes, cx } = useStyles();
+
   return (
-    <>
-      <AssetList />
-    </>
+    <Box sx={{ display: "flex", minHeight: "100dvh" }}>
+      <Sidebar />
+      <Header />
+
+      <Box component="main" className={cx(classes.mainBox, "MainContent")}>
+        <Box
+          sx={{
+            flex: 1,
+            width: "100%",
+            paddingTop: "var(--Header-height)",
+          }}
+        >
+          <StickySubHeader />
+          <Box
+            sx={{
+              padding: "10px",
+            }}
+          >
+            <AssetList />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// import "./index.css";
 import "@fontsource/inter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AssetContextProvider } from "src/context/AssetContext.tsx";
 import { CssBaseline, CssVarsProvider } from "@mui/joy";
+import { StyledEngineProvider } from "@mui/joy/styles";
 
 // React Query can also be used with React's Suspense for Data Fetching API's with its fallbacks
 const client = new QueryClient({
@@ -17,12 +18,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={client}>
       {/* todo: better fallback for whole app */}
       <React.Suspense fallback="Loading...">
-        <CssVarsProvider>
-          <AssetContextProvider>
-            <CssBaseline />
-            <App />
-          </AssetContextProvider>
-        </CssVarsProvider>
+        <AssetContextProvider>
+          <StyledEngineProvider injectFirst>
+            <CssVarsProvider>
+              <CssBaseline />
+              <App />
+            </CssVarsProvider>
+          </StyledEngineProvider>
+        </AssetContextProvider>
       </React.Suspense>
     </QueryClientProvider>
   </React.StrictMode>
