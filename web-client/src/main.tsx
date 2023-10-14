b58bca2d831e062f55ae49007c70c0a18daf6636
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 21:16:04 +0300

    add joyui from mui/joy for FE component lib

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@fontsource/inter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AssetContextProvider } from "src/context/AssetContext.tsx";
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
 
 // React Query can also be used with React's Suspense for Data Fetching API's with its fallbacks
 const client = new QueryClient({
@@ -15,9 +17,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={client}>
       {/* todo: better fallback for whole app */}
       <React.Suspense fallback="Loading...">
-        <AssetContextProvider>
-          <App />
-        </AssetContextProvider>
+        <CssVarsProvider>
+          <AssetContextProvider>
+            <CssBaseline />
+            <App />
+          </AssetContextProvider>
+        </CssVarsProvider>
       </React.Suspense>
     </QueryClientProvider>
   </React.StrictMode>
