131f48c0cc8bba66e2f610471b662fed32b05b1f
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 20:27:03 +0300

    provide context in main.tsx

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AssetContextProvider } from "src/context/AssetContext.tsx";
 
 // React Query can also be used with React's Suspense for Data Fetching API's with its fallbacks
 const client = new QueryClient({
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={client}>
       {/* todo: better fallback for whole app */}
       <React.Suspense fallback="Loading...">
-        <App />
+        <AssetContextProvider>
+          <App />
+        </AssetContextProvider>
       </React.Suspense>
     </QueryClientProvider>
   </React.StrictMode>
