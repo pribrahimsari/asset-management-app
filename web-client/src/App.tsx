bf071ff31d9a5e7a2adef49332179b04a5ac9901
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-14 15:01:15 +0300

    primitive data fetching to work on them

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -2,10 +2,34 @@
 // const ANY_API_URL = import.meta.env.VITE_ANY_API_URL;
 // console.debug({ ANY_API_URL });
 
+import { useQuery } from "@tanstack/react-query";
+
+const fetchAssets = async () => {
+  const response = await fetch("http://localhost:8000/v1/assets?page=1");
+  return response.json();
+};
+
 const App = () => {
+  const { data } = useQuery({
+    queryKey: ["assets"],
+    queryFn: fetchAssets,
+  });
+
+  console.debug({ data });
+
   return (
     <main>
       <h1>Asset Management Application</h1>
+      <ul>
+        {data.data.map((asset) => (
+          <li key={asset.id}>
+            <p>
+              <b>{asset.name}</b>
+            </p>
+            <p>{asset.description}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
