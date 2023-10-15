f0efce54685d76b7bc939cf03ebbb57d7997c9e2
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-15 03:49:29 +0300

    improve AssetList

--- web-client/src/components/AssetList.tsx
+++ web-client/src/components/AssetList.tsx
@@ -1,14 +1,12 @@
-import BasicCreateDeleteButtons from "src/BasicCreateDeleteButtons.tsx";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useAssetContext } from "src/context/AssetContext.tsx";
+import List from "@mui/joy/List";
 
 const AssetList = () => {
   const { assets, isFetching, fetchNextPage, hasNextPage } = useAssetContext();
 
   return (
     <>
-      <BasicCreateDeleteButtons />
-
       <InfiniteScroll
         next={() => !isFetching && fetchNextPage()}
         hasMore={!!hasNextPage}
@@ -29,6 +27,14 @@ const AssetList = () => {
               </li>
             ))}
         </ul>
+        <List
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(500px, 1fr))",
+            gap: 2,
+          }}
+        >
+        </List>
       </InfiniteScroll>
 
       {!hasNextPage && <p>Nothing left to fetch (Total: {assets.length} assets)</p>}
