9ac2eebef0c639408bedf64aa4e5b4167aef1399
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-15 13:47:25 +0300

    display count of the total assets per listed and per all

--- web-client/src/components/AssetStatistics/AssetStatistics.tsx
+++ web-client/src/components/AssetStatistics/AssetStatistics.tsx
@@ -3,20 +3,28 @@ import AssetTypeStatisticsPieChart from "src/components/AssetStatistics/AssetTyp
 import Typography from "@mui/joy/Typography";
 import { AiFillPieChart } from "react-icons/ai";
 import { FaChartPie } from "react-icons/fa";
+import { useMemo } from "react";
 
-// todo responsive
 const AssetStatistics = () => {
   const { listedAssetTypes, allAssetTypes } = useAssetContext();
+
+  const listedAssetsCount = useMemo(() => {
+    return listedAssetTypes.reduce((acc, assetType) => acc + assetType.assets_count, 0);
+  }, [listedAssetTypes]);
+
+  const allAssetsCount = useMemo(() => {
+    return allAssetTypes.reduce((acc, assetType) => acc + assetType.assets_count, 0);
+  }, [allAssetTypes]);
 
   return (
     <div>
       <Typography startDecorator={<AiFillPieChart />} level="title-sm">
-        Types of Listed Assets
+        Types of Listed Assets ({listedAssetsCount})
       </Typography>
       <AssetTypeStatisticsPieChart assetTypes={listedAssetTypes} />
 
       <Typography startDecorator={<FaChartPie />} level="title-sm">
-        Types of All Assets
+        Types of All Assets ({allAssetsCount})
       </Typography>
       <AssetTypeStatisticsPieChart assetTypes={allAssetTypes} />
     </div>
