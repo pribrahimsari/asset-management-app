b654890e7dcf9f3ce77f08fb55db69d82e19233d
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-15 13:37:37 +0300

    add titles to charts

--- web-client/src/components/AssetStatistics/AssetStatistics.tsx
+++ web-client/src/components/AssetStatistics/AssetStatistics.tsx
@@ -1,13 +1,23 @@
 import { useAssetContext } from "src/context/AssetContext.tsx";
 import AssetTypeStatisticsPieChart from "src/components/AssetStatistics/AssetTypeStatisticsPieChart.tsx";
+import Typography from "@mui/joy/Typography";
+import { AiFillPieChart } from "react-icons/ai";
+import { FaChartPie } from "react-icons/fa";
 
-// todo responsive and titles
+// todo responsive
 const AssetStatistics = () => {
   const { listedAssetTypes, allAssetTypes } = useAssetContext();
 
   return (
     <div>
+      <Typography startDecorator={<AiFillPieChart />} level="title-sm">
+        Types of Listed Assets
+      </Typography>
       <AssetTypeStatisticsPieChart assetTypes={listedAssetTypes} />
+
+      <Typography startDecorator={<FaChartPie />} level="title-sm">
+        Types of All Assets
+      </Typography>
       <AssetTypeStatisticsPieChart assetTypes={allAssetTypes} />
     </div>
   );
