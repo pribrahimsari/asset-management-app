133d8cf3d2e87664f10ea7ba6fb751f5aed9832c
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-10-15 20:40:06 +0300

    create tag and note resource and use in getAssets

--- api/app/Http/Resources/AssetResource.php
+++ api/app/Http/Resources/AssetResource.php
@@ -20,6 +20,8 @@ class AssetResource extends JsonResource
             "description"=>$this->description,
             "type_id"=>$this->type_id,
             "type"=>TypeResource::make($this->type),
+            "tags"=>TagResource::collection($this->tags),
+            "notes"=>NoteResource::collection($this->notes),
             "addition_time"=>$this->addition_time,
             "created_at"=>$this->created_at,
             "priority"=>$this->priority,
